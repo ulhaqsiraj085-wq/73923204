1bbf3162f3a356414dc2e85d42a2aff53d9b5593
ulhaqsiraj085-wq <ulhaqsiraj085@gmail.com>
2026-05-31 13:26:00 +0500

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,13 @@
+const posts = [
+  {
+    title: "India vs Pakistan – Epic Match Review",
+    date: "2026-05-31",
+    content: "Aaj ka match bohat zabardast tha. India ne last over mein jeet hasil ki..."
+  },
+
+  {
+    title: "Top Players of the Day",
+    date: "2026-05-30",
+    content: "Babar Azam aur Kohli ne shandar performance di..."
+  }
+];
